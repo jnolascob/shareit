8b7cdf535556890e8baa8009292468c983cd3a1f
jnolascob <jnolascob@gmail.com>
2014-08-31 21:06:54 -0500

    modify upload photo

--- webservices/upload_photo.php
+++ webservices/upload_photo.php
@@ -3,20 +3,26 @@ print_r($_FILES);
 $response = array();
 
 /*paths*/
-$uploaddir = '/var/www/html/static/';
+$success = 0;
+$message = "Pendiente";
+$uploaddir = '/var/www/html/static/images';
 $uploadfile = $uploaddir . basename($_FILES['file']['name']);
 
 $new_image_name = "_".rand()."_".rand()."images.jpg";
 //move_uploaded_file($_FILES["file"]["tmp_name"], "/static/images/");
 if(move_uploaded_file($_FILES["file"]["tmp_name"], $uploadfile)){
 	echo "successfully uploaded";
+	$success = 1;
+	$message = "Uploaded";
 }
 else{
 	echo "Upload failed";
+	$success = 0;
+	$message = "Upload failed";
 }
 $response['status'] = 200;
-$response['success'] = 1;
-$response['message'] = "Se guardo";
+$response['success'] = $success;
+$response['message'] = $message;
 $response['route'] = $uploadfile;
 echo json_encode($response);
 
