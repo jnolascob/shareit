4729bee19d1a1220e164dd2729813d57d699a0f2
jnolascob <jnolascob@gmail.com>
2014-08-31 21:02:52 -0500

    update ws upload photo

--- webservices/upload_photo.php
+++ webservices/upload_photo.php
@@ -2,13 +2,22 @@
 print_r($_FILES);
 $response = array();
 
+/*paths*/
+$uploaddir = '/var/www/html/static/';
+$uploadfile = $uploaddir . basename($_FILES['file']['name']);
+
 $new_image_name = "_".rand()."_".rand()."images.jpg";
 //move_uploaded_file($_FILES["file"]["tmp_name"], "/static/images/");
-move_uploaded_file($_FILES["file"]["tmp_name"], "/var/www/html/static/images/");
+if(move_uploaded_file($_FILES["file"]["tmp_name"], $uploadfile)){
+	echo "successfully uploaded";
+}
+else{
+	echo "Upload failed";
+}
 $response['status'] = 200;
 $response['success'] = 1;
 $response['message'] = "Se guardo";
-$response['route'] = "http://104.131.36.201/static/images/";
+$response['route'] = $uploadfile;
 echo json_encode($response);
 
 ?>
