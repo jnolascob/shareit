a2b3ed57453e8e49f57bdc9c79aadc1878b58295
jnolascob <jnolascob@gmail.com>
2014-08-31 21:45:48 -0500

    update upload audios

--- webservices/upload_audio.php
+++ webservices/upload_audio.php
@@ -1,33 +1,31 @@
 <?php
-require_once '../model/Imagenes.php';
-$I = new Imagenes();
-// Where the file is going to be placed
-$target_path = "/home/siont/public_html/ws/static/audios/";
- 
-/* Add the original filename to our target path.
-Result is "uploads/filename.extension" */
-$target_path = $target_path . basename( $_FILES['file']['name']);
+print_r($_FILES);
+$path = $_FILES['file']['name'];
+$ext = pathinfo($path, PATHINFO_EXTENSION);
+echo "<br>".$ext;
 
-$new_audio_name = "_".rand()."_".rand()."audio.3gpp";
-move_uploaded_file($_FILES["file"]["tmp_name"], "/home/siont/public_html/ws/static/audios/".$new_audio_name);
-//move_uploaded_file($_FILES['file']['tmp_name'], $target_path);
+$response = array();
 
-$audio_descripcion = "descripcion";
-$audio_nombre = $new_audio_name;
-$audio_url = "http://www.siont.com.co/ws/static/audio/".$audio_nombre;
-$audio_like = 0;
-$usuario_id = $_POST['usuario_id'];
+/*paths*/
+$success = 0;
+$message = "Pendiente";
+$add_name = date('Ymd_His')."_".rand().".mp3";
+$uploaddir = '/var/www/html/static/audios/';
 
-$result = $I->upload_audio($audio_nombre,$audio_descripcion,$audio_url,$audio_like,$usuario_id);
+$uploadfile = $uploaddir.$add_name;
 
-print_r($result);
-/*
-if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
-    echo "The file ".  basename( $_FILES['file']['name']).
-    " has been uploaded";
-} else{
-    echo "There was an error uploading the file, please try again!";
-    echo "filename: " .  basename( $_FILES['file']['name']);
-    echo "target_path: " .$target_path;
-}*/
+if(move_uploaded_file($_FILES["file"]["tmp_name"], $uploadfile)){
+	$success = 1;
+	$message = "Upload success";
+}
+else{
+	$success = 0;
+	$message = "Upload failed";
+}
+$response['status'] = 200;
+$response['success'] = $success;
+$response['message'] = $message;
+$response['route'] = $uploadfile;
+$response['name'] = $add_name;
+echo json_encode($response);
 ?>
