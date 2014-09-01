defac9c47c7563eec7271c69c21ab97bb4f2e912
jnolascob <jnolascob@gmail.com>
2014-08-31 21:39:58 -0500

    add data and random

--- webservices/upload_photo.php
+++ webservices/upload_photo.php
@@ -5,11 +5,11 @@ $response = array();
 /*paths*/
 $success = 0;
 $message = "Pendiente";
+$add_name = date('Ymd_His')."_".rand().".jpg";
 $uploaddir = '/var/www/html/static/images/';
-$uploadfile = $uploaddir . basename($_FILES['file']['name']);
+//$uploadfile = $uploaddir . basename($_FILES['file']['name']);
+$uploadfile = $uploaddir.$add_name;
 
-$new_image_name = "_".rand()."_".rand()."images.jpg";
-//move_uploaded_file($_FILES["file"]["tmp_name"], "/static/images/");
 if(move_uploaded_file($_FILES["file"]["tmp_name"], $uploadfile)){
 	$success = 1;
 	$message = "Upload success";
@@ -22,12 +22,6 @@ $response['status'] = 200;
 $response['success'] = $success;
 $response['message'] = $message;
 $response['route'] = $uploadfile;
-$response['name'] = $uploadfile;
+$response['name'] = $add_name;
 echo json_encode($response);
-echo date('Y-m-d H:i:s');
-echo "<br>";
-echo date('Ymd');
-echo "<br>";
-echo date('Y_m_d_H_i_s');
-
 ?>
